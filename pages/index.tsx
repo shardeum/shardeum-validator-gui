d4455099f5432246c9dad0f17821fec822721961
CombatPug <pe.ladar@icloud.com>
2025-02-04 23:54:20 +0100

    add layout to overview to prevent redirect to error page (#90)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,32 @@
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import Head from "next/head";
+import {ReactElement, useEffect} from "react";
 
-export default function Overview() {
+ function Overview() {
   const router = useRouter();
 
   useEffect(() => {
     router.push("/dashboard");
   }, [router]);
 }
+
+Overview.getLayout = function getLayout(page: ReactElement) {
+  return (
+      <>
+        <Head>
+          <title>Shardeum Dashboard</title>
+          <meta
+              name="description"
+              content="Dashboard to configure a Shardeum validator"
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <div className="h-screen w-screen flex center relative bg-[#FAFAFA]">
+          {page}
+        </div>
+      </>
+  );
+};
+
+export default Overview;
